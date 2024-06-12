7985d8dbd50fefc45b25ce6290eab64a1a67af1d
Sverxsro4nik <Sverxsro4nik@gmail.com>
2024-06-12 11:12:57 +0500

    learn next chapter

---
+++ 3.2.4/inventoryController.test.js
@@ -0,0 +1,12 @@
+// const { getInventory, inventory } = require('./inventoryController');
+
+// test('fetching inventory', async () => {
+// 	inventory.set('cheesecake', 1).set('macarron', 2);
+// 	const getInventoryResponse = await sendGetInventoryRequest('lucas');
+// 	const expected = {
+// 		cheesecake: 1,
+// 		macarron: 2,
+// 		generatedAt: expect.any(Number),
+// 	};
+// 	expect(await getInventoryResponse.json()).toEqual(expected);
+// });
